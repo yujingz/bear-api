490d8ae547aa4f0a5299aa09f016e724445876e5
Yujing Zheng <yujingz@me.com>
2015-01-10 12:50:50 -0600

    add idea up and down

--- app/api/api.rb
+++ app/api/api.rb
@@ -16,6 +16,47 @@ class API < Grape::API
   end
 
   params do
+    requires :token
+  end
+  get 'ideas/:id' do
+    authenticate!
+    idea = Idea.find(params[:id])
+    if idea.present?
+      idea
+    else
+      error!({error: "cant find such idea"}, 404)
+    end
+  end
+
+  params do
+    requires :token
+  end
+  post 'ideas/:id/up' do
+    authenticate!
+    idea = Idea.find(params[:id])
+    if idea.present?
+      idea.score = idea.score + 1
+      idea.save
+    else
+      error!({error: "cant find such idea"}, 404)
+    end
+  end
+
+  params do
+    requires :token
+  end
+  get 'ideas/:id/down' do
+    authenticate!
+    idea = Idea.find(params[:id])
+    if idea.present?
+      idea.score = idea.score - 1
+      idea.save
+    else
+      error!({error: "cant find such idea"}, 404)
+    end
+  end
+
+  params do
     requires :username
     requires :password
     requires :password_confirmation
